08d9fa628966658de50ef6ae807ff8b1e6a996b4
JustinC270 <justincaamano770@gmail.com>
2025-10-14 13:30:23 +0200

    arrays

---
+++ IntroduccionArrays.java
@@ -0,0 +1,14 @@
+public class IntroduccionArrays {
+    public static void main(String[] args) {
+        String[] maxTemperaturas = new String[7];
+        maxTemperaturas[0] = "Lunes";
+        maxTemperaturas[1] = "Martes";
+        maxTemperaturas[2] = "Miercoles";
+        maxTemperaturas[3] = "Jueves";
+        maxTemperaturas[4] = "Viernes";
+        maxTemperaturas[5] = "Sabado";
+        maxTemperaturas[6] = "Domingo";
+        System.out.printf("Primer dia: %s\n", maxTemperaturas[0]);
+    }
+
+}
